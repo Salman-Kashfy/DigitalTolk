6b3d1d5587902533bcb9f51f573addd24804018c
Salman Kashfy <salmankashfy@gmail.com>
2025-06-22 18:09:42 +0500

    Add info

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Modules\Translation\Http\Controllers\TranslationController;
 use App\Modules\Auth\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Config;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/health', function (Request $request) {
-    return response()->json(['status' => true, 'message' => 'Hello world!']);
+    $defaultConnection = Config::get('database.default'); // This should be 'mysql' as per your setup
+    $databaseName = Config::get("database.connections.{$defaultConnection}.database"); // Should be 'laravel_docker'
+    $databaseUser = Config::get("database.connections.{$defaultConnection}.username"); // Should be 'user'
+    $databaseHost = Config::get("database.connections.{$defaultConnection}.host"); // This will typically be 'mysql' (the service name in docker-compose)
+
+    return response()->json([
+        'status' => true,
+        'message' => 'Hello world!',
+        'database_name' => $databaseName,
+        'database_user' => $databaseUser, // <--- Add this line
+        'database_host' => $databaseHost  // <--- Add this line
+    ]);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
